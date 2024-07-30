131107ac2f05d1266ad1a1f466d1e7934d3c9617
qls0439 <qls0439@gmtc.kr>
2024-07-30 18:31:08 +0900

    theme 변경

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import styled, {keyframes} from "styled-components";
 
+const Title = styled.h1`
+    color: ${props => props.theme.textColor};
+`;
+
 const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
+  background-color: ${props => props.theme.backgroundColor};
 `;
 
 const Emogi = styled.span`
@@ -41,9 +46,7 @@ const Box = styled.div`
 function App() {
     return (
         <Wrapper>
-            <Box>
-                <Emogi>😊</Emogi>
-            </Box>
+            <Title>Hello</Title>
         </Wrapper>
     );
 }
